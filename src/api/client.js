fbd2211835aec3c857f9c4cc28ef8765aa30fe00
EboTheFirst <ebokak5@gmail.com>
2024-11-29 09:41:40 +0000

    changed start command to hosting

--- src/api/client.js
+++ src/api/client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const client = axios.create({
-  baseURL: "http://54.246.247.31:8000",
+  baseURL: "http://54.246.247.31:8000", // For EC2, run backend in prod mode; " fastapi run "
   // baseURL: "http://localhost:8000",
 });
 
